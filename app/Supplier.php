2c92e929456bae2efdd5b0e337c8ad6962bbb5ff
armage <armage@gmail.com>
2018-12-07 14:24:28 -0300

    Supplier Model and migration

---
+++ app/Supplier.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Supplier extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+    protected $fillable = ['taxNumber', 'bussinesName', 'name', 'abbreviation'];
+
+
+
+
+    public function addresses()
+    {
+        return $this->morphMany('App\Address', 'addressable');
+    }
+    public function contacts()
+    {
+        return $this->morphMany('App\Contact', 'contactable');
+    }
+}
